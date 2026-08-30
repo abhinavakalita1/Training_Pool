f815d53b65bf816f9089fba15231e1297b68b4af
SDivy1811 <divysingh332@gmail.com>
2026-08-30 12:44:50 +0530

    aruco_plus_cleanup

--- build/uavcollab/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/media/divy/2454fe4a-d2e4-460e-b4f5-df3cde6b9592/training_pool/install/uavcollab'
