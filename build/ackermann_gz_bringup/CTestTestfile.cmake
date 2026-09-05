03da02a05408a5f1ae35c172a2366996c6fe7000
jashanpreet singh <jashanpreetsinghbrar01@gmail.com>
2026-09-05 17:01:15 +0530

    chore: add .gitignore and remove build/install/log artifacts

--- build/ackermann_gz_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jashan/training_pool/src/ackermann_gz_bringup
-# Build directory: /home/jashan/training_pool/build/ackermann_gz_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/drdo_gz_worlds/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jashan/training_pool/src/drdo_gz_worlds
-# Build directory: /home/jashan/training_pool/build/drdo_gz_worlds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
