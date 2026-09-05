4036fa9b9714f78645649436934e6d72a38dd36b
drdo <drdo@local>
2026-09-05 15:55:51 +0530

    B4a: drop 1e-6 include scale

---
+++ build/drdo_gz_worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jashan/training_pool/src/drdo_gz_worlds
+# Build directory: /home/jashan/training_pool/build/drdo_gz_worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
